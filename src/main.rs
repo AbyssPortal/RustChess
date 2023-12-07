a4d953efdd4a8c0d289a227b0ba5aba227835782
DarkPortal <planteraminehut@gmail.com>
2023-12-07 19:39:23 +0200

    game now ends on checkmate

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,32 @@ use text_io::*;
 mod chess;
 mod chess_io;
 
-
 fn main() {
     use chess::chess::*;
-    let mut def_board = make_board_from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/ w KQkq - 0 1").unwrap();
-    loop {
+    let mut def_board =
+        make_board_from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/ w KQkq - 0 1").unwrap();
+
+    while def_board.is_checkmate().is_none() {
         def_board.print_board();
-        match def_board
-            .make_legal_move(ChessMove {
-                initial_row: read!(),
-                initial_col: read!(),
-                destination_row: read!(),
-                destination_col: read!(),
-            }) {
-                Err(BoardError::IllegalMoveError) => {println!("That's illegal silly!");}
-                Err(BoardError::NoPieceError) => {println!("There's no piece there silly!");}
-                Err(BoardError::OutOfBoundsError) => {print!("That's out of bounds silly!");}
+        let move_string: String = read!();
+        match def_board.interpert_move(&move_string) {
+            Ok(chess_move) => match def_board.make_legal_move(chess_move) {
+                Err(BoardError::IllegalMoveError) => {
+                    println!("That's illegal silly!");
+                }
+                Err(BoardError::NoPieceError) => {
+                    println!("There's no piece there silly!");
+                }
+                Err(BoardError::OutOfBoundsError) => {
+                    print!("That's out of bounds silly!");
+                }
                 Ok(()) => {}
+            },
+            Err(err) => {
+                println!("{:?}", err);
             }
+        }
     }
+    def_board.print_board();
+    println!("{} is checkmated!", def_board.is_checkmate().expect(":/").to_string());
 }
